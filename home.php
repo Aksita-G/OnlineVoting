703c803ea977d81e01ee3282e34c5e447d4d7f99
Aksita-G <113622172+Aksita-G@users.noreply.github.com>
2023-12-11 12:15:38 +0530

    Update home.php

--- home.php
+++ home.php
@@ -1,221 +1,78 @@
-<?php include 'https://mystorageaccou.blob.core.windows.net/mycontainer/session.php'; ?>
-<?php include 'https://mystorageaccou.blob.core.windows.net/mycontainer/header.php'; ?>
-<body class="hold-transition skin-blue layout-top-nav">
-<div class="wrapper">
+<?php
+session_start();
 
-	<?php include 'https://mystorageaccou.blob.core.windows.net/mycontainer/navbar.php'; ?>
-	 
-	  <div class="content-wrapper">
-	    <div class="container">
+if (isset($_SESSION['admin'])) {
+    header('location: admin/home.php');
+}
 
-	      <!-- Main content -->
-	      <section class="content">
-	      	<?php
-	      		$parse = parse_ini_file('admin/config.ini', FALSE, INI_SCANNER_RAW);
-    			$title = $parse['election_title'];
-	      	?>
-	      	<h1 class="page-header text-center title"><b><?php echo strtoupper($title); ?></b></h1>
-	        <div class="row">
-	        	<div class="col-sm-10 col-sm-offset-1">
-	        		<?php
-				        if(isset($_SESSION['error'])){
-				        	?>
-				        	<div class="alert alert-danger alert-dismissible">
-				        		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-					        	<ul>
-					        		<?php
-					        			foreach($_SESSION['error'] as $error){
-					        				echo "
-					        					<li>".$error."</li>
-					        				";
-					        			}
-					        		?>
-					        	</ul>
-					        </div>
-				        	<?php
-				         	unset($_SESSION['error']);
+if (isset($_SESSION['voter'])) {
+    header('location: home.php');
+}
 
-				        }
-				        if(isset($_SESSION['success'])){
-				          	echo "
-				            	<div class='alert alert-success alert-dismissible'>
-				              		<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-				              		<h4><i class='icon fa fa-check'></i> Success!</h4>
-				              	".$_SESSION['success']."
-				            	</div>
-				          	";
-				          	unset($_SESSION['success']);
-				        }
+// Function to download the file content from Azure Blob Storage
+function downloadAzureFile($url)
+{
+    $content = file_get_contents($url);
+    if ($content === false) {
+        // Handle error, e.g., log it or display a message
+        die("Failed to download file from Azure Blob Storage");
+    }
+    return $content;
+}
 
-				    ?>
- 
-				    <div class="alert alert-danger alert-dismissible" id="alert" style="display:none;">
-		        		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-			        	<span class="message"></span>
-			        </div>
+// Download header.php content
+$headerContent = downloadAzureFile('https://mystorageaccou.blob.core.windows.net/mycontainer/header.php');
 
-				    <?php
-				    	$sql = "SELECT * FROM votes WHERE voters_id = '".$voter['id']."'";
-				    	$vquery = $conn->query($sql);
-				    	if($vquery->num_rows > 0){
-				    		?>
-				    		<div class="text-center">
-					    		<h3>You have already voted for this election.</h3>
-					    		<a href="#view" data-toggle="modal" class="btn btn-flat btn-primary btn-lg">View Ballot</a>
-					    	</div>
-				    		<?php
-				    	}
-				    	else{
-				    		?>
-			    			<!-- Voting Ballot -->
-						    <form method="POST" id="ballotForm" action="submit_ballot.php">
-				        		<?php
-				        			include 'https://mystorageaccou.blob.core.windows.net/mycontainer/slugify.php';
+// Download scripts.php content
+$scriptsContent = downloadAzureFile('https://mystorageaccou.blob.core.windows.net/mycontainer/scripts.php');
+?>
 
-				        			$candidate = '';
-				        			$sql = "SELECT * FROM positions ORDER BY priority ASC";
-									$query = $conn->query($sql);
-									while($row = $query->fetch_assoc()){
-										$sql = "SELECT * FROM candidates WHERE position_id='".$row['id']."'";
-										$cquery = $conn->query($sql);
-										while($crow = $cquery->fetch_assoc()){
-											$slug = slugify($row['description']);
-											$checked = '';
-											if(isset($_SESSION['post'][$slug])){
-												$value = $_SESSION['post'][$slug];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Voting System</title>
+    <!-- Include downloaded header content -->
+    <?php echo $headerContent; ?>
+</head>
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <b>Voting System</b>
+        </div>
+        <div class="login-box-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+            <form action="login.php" method="POST">
+                <div class="form-group has-feedback">
+                    <input type="text" class="form-control" name="voter" placeholder="Voter's ID" required>
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" class="form-control" name="password" placeholder="Password" required>
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <?php
+        if (isset($_SESSION['error'])) {
+            echo "
+                <div class='callout callout-danger text-center mt20'>
+                    <p>" . $_SESSION['error'] . "</p> 
+                </div>
+            ";
+            unset($_SESSION['error']);
+        }
+        ?>
+    </div>
 
-												if(is_array($value)){
-													foreach($value as $val){
-														if($val == $crow['id']){
-															$checked = 'checked';
-														}
-													}
-												}
-												else{
-													if($value == $crow['id']){
-														$checked = 'checked';
-													}
-												}
-											}
-											$input = ($row['max_vote'] > 1) ? '<input type="checkbox" class="flat-red '.$slug.'" name="'.$slug."[]".'" value="'.$crow['id'].'" '.$checked.'>' : '<input type="radio" class="flat-red '.$slug.'" name="'.slugify($row['description']).'" value="'.$crow['id'].'" '.$checked.'>';
-											$image = (!empty($crow['photo'])) ? 'images/'.$crow['photo'] : 'images/profile.jpg';
-											$candidate .= '
-												<li>
-													'.$input.'<button type="button" class="btn btn-primary btn-sm btn-flat clist platform" data-platform="'.$crow['platform'].'" data-fullname="'.$crow['firstname'].' '.$crow['lastname'].'"><i class="fa fa-search"></i> Platform</button><img src="'.$image.'" height="100px" width="100px" class="clist"><span class="cname clist">'.$crow['firstname'].' '.$crow['lastname'].'</span>
-												</li>
-											';
-										}
-
-										$instruct = ($row['max_vote'] > 1) ? 'You may select up to '.$row['max_vote'].' candidates' : 'Select only one candidate';
-
-										echo '
-											<div class="row">
-												<div class="col-xs-12">
-													<div class="box box-solid" id="'.$row['id'].'">
-														<div class="box-header with-border">
-															<h3 class="box-title"><b>'.$row['description'].'</b></h3>
-														</div>
-														<div class="box-body">
-															<p>'.$instruct.'
-																<span class="pull-right">
-																	<button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="'.slugify($row['description']).'"><i class="fa fa-refresh"></i> Reset</button>
-																</span>
-															</p>
-															<div id="candidate_list">
-																<ul>
-																	'.$candidate.'
-																</ul>
-															</div>
-														</div>
-													</div>
-												</div>
-											</div>
-										';
-
-										$candidate = '';
-
-									}	
-
-				        		?>
-				        		<div class="text-center">
-					        		<button type="button" class="btn btn-success btn-flat" id="preview"><i class="fa fa-file-text"></i> Preview</button> 
-					        		<button type="submit" class="btn btn-primary btn-flat" name="vote"><i class="fa fa-check-square-o"></i> Submit</button>
-					        	</div>
-				        	</form>
-				        	<!-- End Voting Ballot -->
-				    		<?php
-				    	}
-
-				    ?>
-
-	        	</div>
-	        </div>
-	      </section>
-	     
-	    </div>
-	  </div>
-  
-  	<?php include 'https://mystorageaccou.blob.core.windows.net/mycontainer/footer.php'; ?>
-  	<?php include 'https://mystorageaccou.blob.core.windows.net/mycontainer/ballot_modal.php'; ?>
-</div>
-
-<?php include 'https://mystorageaccou.blob.core.windows.net/mycontainer/scripts.php'; ?>
-<script>
-$(function(){
-	$('.content').iCheck({
-		checkboxClass: 'icheckbox_flat-green',
-		radioClass: 'iradio_flat-green'
-	});
-
-	$(document).on('click', '.reset', function(e){
-	    e.preventDefault();
-	    var desc = $(this).data('desc');
-	    $('.'+desc).iCheck('uncheck');
-	});
-
-	$(document).on('click', '.platform', function(e){
-		e.preventDefault();
-		$('#platform').modal('show');
-		var platform = $(this).data('platform');
-		var fullname = $(this).data('fullname');
-		$('.candidate').html(fullname);
-		$('#plat_view').html(platform);
-	});
-
-	$('#preview').click(function(e){
-		e.preventDefault();
-		var form = $('#ballotForm').serialize();
-		if(form == ''){
-			$('.message').html('You must vote atleast one candidate');
-			$('#alert').show();
-		}
-		else{
-			$.ajax({
-				type: 'POST',
-				url: 'preview.php',
-				data: form,
-				dataType: 'json',
-				success: function(response){
-					if(response.error){
-						var errmsg = '';
-						var messages = response.message;
-						for (i in messages) {
-							errmsg += messages[i]; 
-						}
-						$('.message').html(errmsg);
-						$('#alert').show();
-					}
-					else{
-						$('#preview_modal').modal('show');
-						$('#preview_body').html(response.list);
-					}
-				}
-			});
-		}
-		
-	});
-
-});
-</script>
+    <!-- Include downloaded scripts content -->
+    <?php echo $scriptsContent; ?>
 </body>
 </html>
